ae6fb22094d3c844b893d03f92093c9e1e03f6e7
salemzii <salemododa2@gmail.com>
2022-02-28 11:38:09 +0100

    included requirements.txt

--- ThePeer/client.py
+++ ThePeer/client.py
@@ -14,34 +14,34 @@ class Client():
     api_key : str
     
 
-    request = requests.Session()
-    base_endpoint = "https://api.thepeer.co/"
+    _request = requests.Session()
+    _base_endpoint = "https://api.thepeer.co/"
 
     def set_auth_headers(self):
         headers = {
             "X-Api-Key": self.api_key,
             "Accept": "application/json"
         }
-        return self.request.headers.update(headers)
+        return self._request.headers.update(headers)
 
     
     def get_users(self) -> dict:
-        path = self.base_endpoint + "users"
+        path = self._base_endpoint + "users"
         self.set_auth_headers()
         try:
-            req = self.request.get(path)
+            req = self._request.get(path)
             return req
         except Exception as err:
             return err
 
     
     def create_user(self, data : dict) -> object:
-        path = self.base_endpoint + "users"
+        path = self._base_endpoint + "users"
         self.set_auth_headers()
 
         if Validator(data, create_user_schema_conf).validate():
             try:
-                req = self.request.post(path, data=data)
+                req = self._request.post(path, data=data)
                 return req
             except Exception as err:
                 return err
@@ -51,11 +51,11 @@ class Client():
 
     def update_user(self, reference:str, data:dict) -> object:
         
-        path = self.base_endpoint + f"users/{reference}"
+        path = self._base_endpoint + f"users/{reference}"
         self.set_auth_headers()
         if Validator(data, update_user_schema_conf).validate():
             try:
-                req = self.request.put(path, data=data)
+                req = self._request.put(path, data=data)
                 return req
             except Exception as err:
                 return err
@@ -63,10 +63,10 @@ class Client():
             raise "invalid post data"
     
     def delete_user(self, reference:str) -> object:
-        path = self.base_endpoint + f"users/{reference}"
+        path = self._base_endpoint + f"users/{reference}"
         self.set_auth_headers()
         try:
-            req = self.request.delete(path)
+            req = self._request.delete(path)
             return req
         except Exception as err:
             return err             
@@ -78,68 +78,68 @@ class Client():
 
     def get_transaction(self, transactionId) -> dict:
 
-        path = self.base_endpoint +f"transactions/{transactionId}"
+        path = self._base_endpoint +f"transactions/{transactionId}"
         self.set_auth_headers()
         try:
-            req = self.request.get(path)
+            req = self._request.get(path)
             return req
         except Exception as err:
             return err
 
     def refund_transaction(self, transactionId, data=None) -> json:
-        path = self.base_endpoint +f"transactions/{transactionId}/refund"
+        path = self._base_endpoint +f"transactions/{transactionId}/refund"
         self.set_auth_headers()
 
         try:
             if data != None and Validator(data, transaction_refund_schema_conf):
-                req = self.request.post(path, data=data)
+                req = self._request.post(path, data=data)
                 return json.loads(req.text)
-            req = self.request.post(path, data={"reason":"possible fraud"})
+            req = self._request.post(path, data={"reason":"possible fraud"})
             return req
         except Exception as err:
             return err
 
     def get_user_links(self, reference: str) -> json:
-        path = self.base_endpoint + f"users/{reference}/links"
+        path = self._base_endpoint + f"users/{reference}/links"
         self.set_auth_headers()
 
         try:
-            req = self.request.get(path)
+            req = self._request.get(path)
             return req
 
         except Exception as err:
             return err
 
     def get_user_link(self, linkId: str) -> json:
-        path = self.base_endpoint + f"link/{linkId}"
+        path = self._base_endpoint + f"link/{linkId}"
         self.set_auth_headers()
 
         try:
-            req = self.request.get(path)
+            req = self._request.get(path)
             return req
         except Exception as err:
             return err
 
     def test_recieve(self, data=None) -> json:
-        path = self.base_endpoint + f"test-receive"
+        path = self._base_endpoint + f"test-receive"
         self.set_auth_headers()
 
         try:
             if data != None and Validator(data, test_recieve_schema_conf).validate():
-                req = self.request.post(path,data=data)
+                req = self._request.post(path,data=data)
                 return req
-            req = self.request.post(path, data={"amount": 10000,"user_reference": "banana-island-ikoyi"})
+            req = self._request.post(path, data={"amount": 10000,"user_reference": "banana-island-ikoyi"})
             return req
         except Exception as err:
             return err
     
 
     def charge(self, linkId:str, data:dict) -> json:
-        path = self.base_endpoint + f"link/{linkId}/charge"
+        path = self._base_endpoint + f"link/{linkId}/charge"
 
         if Validator(data, charge_schema_conf).validate():
             try:
-                req = self.request.post(path, data=data)
+                req = self._request.post(path, data=data)
                 return req
             except Exception as err:
                 return err
